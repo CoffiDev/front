08093788d24e4230ffc7f481ea14c40725c65445
Coffi <alexcuenca4@gmail.com>
2018-10-07 04:29:46 -0500

    Add random image

--- src/components/SuccessCases.vue
+++ src/components/SuccessCases.vue
@@ -10,7 +10,7 @@
       </span>
     </p>
     <div class="patient-photo">
-      <img src="" :alt='patientName'/>
+      <img :src="img" :alt='patientName' class="child"/>
     </div>
     <p class='text-read text-read--small'>
       <small>
@@ -21,8 +21,37 @@
 </template>
 
 <script>
+import img1 from '../assets/photos/n1.jpg'
+import img2 from '../assets/photos/n2.jpg'
+import img3 from '../assets/photos/n3.png'
+import img4 from '../assets/photos/n4.jpg'
+
+
+const randomImage = () => {
+  const random = Math.ceil(Math.random() * 100)
+
+  console.log(random)
+
+  if (random > 0 && random <= 25) {
+    return  img1
+  } else if (random > 25 && random <= 50) {
+    return img2
+  } else if (random > 50 && random <= 75) {
+    return img3
+  } else if (random > 75 && random <= 100) {
+    return img4
+  } else {
+    return img1
+  }
+}
+
 export default {
   name: 'SuccessCases',
+  data() {
+    return {
+      img: randomImage()
+    }
+  },
   props: {
     patientName: {
       type: String,
@@ -42,3 +71,10 @@ export default {
   }
 }
 </script>
+
+<style lang="sass" scoped>
+  .child
+    width: 200px
+    height: 200px
+    border-radius: 50%
+</style>
