226d922550f579bc41b060be7dde6206a962d6e0
Miguel Ángel Arroyo Rosales <miguel@ruidovisual.com>
2018-10-07 00:34:34 -0500

    add success cases

---
+++ src/components/SuccessCases.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="text-center">
+    <p>
+      <strong class='text-emphasis'>{{patientName}}</strong>
+      <br/>
+      así como {{patients}} niños con {{ailmentName}}
+      <br/>
+      <span class='text-emphasis text-emphasis--medium'>
+        han <strong> mejorado su vida </strong> gracias a ti
+      </span>
+    </p>
+    <div class="patient-photo">
+      <img src="" :alt='patientName'/>
+    </div>
+    <p class='text-read text-read--small'>
+      <small>
+        Andrés Aprendió a comportarse mejor,  ya espera turnos, empieza a solicitar las cosas, a vestirse solo,  así como a ir al baño solo. 
+      </small>
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'SuccessCases',
+  props: {
+    patientName: {
+      type: String,
+      default: 'Andres'
+    }, 
+    patients: {
+      type: Number, 
+      default: 55
+    }, 
+    ailmentName: {
+      type: String,
+      default: 'autismo'
+    }
+
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,7 @@
       :goalChildren=100000 />
     <Hero/>
     <Patients/>
+    <SuccessCases/>
   </div>
 </template>
 
@@ -15,7 +16,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 import Header from '@/components/Header.vue' 
 import Hero from '@/components/Hero.vue' 
 import Patients from '@/components/Patients.vue' 
-
+import SuccessCases from '@/components/SuccessCases.vue' 
 import { PatientsContainer } from '@/containers/PatientsContainer'
 import { HeroContainer } from '@/containers/HeroContainer'
 
@@ -27,6 +28,7 @@ export default {
     HelloWorld, 
     Hero: HeroContainer(Hero),
     Patients: PatientsContainer(Patients),
+    SuccessCases,
   }
 }
 </script>
