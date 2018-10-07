9ee1d34a165923678ae460de739cbbbde6d5345c
Coffi <alexcuenca4@gmail.com>
2018-10-07 11:25:41 -0500

    Add container

---
+++ src/components/CRITInfo.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="crit-info">
+    <information-nugget
+      :lines="[
+        { text: benefitChildren.toLocaleString(), type: 'accent' },
+        { text: 'niños beneficiados', type: 'emphasis' },
+        { text: 'con tu donativo' }
+      ]"
+    />
+
+    <information-nugget
+      :lines="[
+        { text: 'Los donativos representan', type: 'emphasis' },
+        { text: donationsRepresentationPercentage + '%', type: 'accent' },
+        { text: 'de los recursos que necesita este centro', type: 'emphasis' }
+      ]"
+    />
+
+    <information-nugget
+      :lines="[
+        { text: 'Su costo por paciente asciende a', type: 'emphasis' },
+        { text: '$' + costPerpatient.toLocaleString(), type: 'accent' }
+      ]"
+    />
+
+    <information-nugget
+      :lines="[
+        { text: 'El centro necesita', type: 'emphasis' },
+        { text: '$' + Math.ceil(maxAmountRequired / 1000000) + 'MDP', type: 'accent'},
+        { text: 'para atender hasta' },
+        { text: maxAmountPatients, type: 'accent' },
+        { text: 'niños y niñas' },
+      ]"
+    />
+  </div>
+</template>
+
+<script>
+import InformationNugget from './InformationNugget'
+
+export default {
+  name: "CRITInfo",
+  components: {
+    InformationNugget
+  },
+  props: {
+    benefitChildren: {
+      type: Number,
+      default: 1147
+    },
+    donationsRepresentationPercentage: {
+      type: Number,
+      default: 50
+    },
+    costPerpatient: {
+      type: Number,
+      default: 35521
+    },
+    maxAmountRequired: {
+      type: Number,
+      default: 82
+    },
+    maxAmountPatients: {
+      type: Number,
+      default: 2000
+    }
+  }
+}
+</script>
---
+++ src/components/InformationNugget.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="informationNugget" style="display: flex; flex-direction: column; align-items: center; padding-bottom: 30px">
+    <div v-for="line in lines" :class="line.type || 'text'">
+        {{line.text}}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "InformationNugget",
+    props: {
+      lines: {
+        type: Array,
+        default: () => [],
+        shape: { text: String, type: ['accent', 'emphasis', 'text'] }
+      }
+    }
+  }
+</script>
+
+<style lang="sass" scoped>
+  .accent
+    color: var(--color-accent)
+    font-size: 24px
+    font-weight: bold
+
+  .text
+    color: var(--color-primary)
+    font-size: 18px
+
+  .emphasis
+    color: var(--color-primary)
+    font-size: 18px
+    font-weight: bold
+</style>
