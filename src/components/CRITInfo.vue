35ea34155a84531f3deb8f598b9f4f076a6ee8a5
Miguel Ángel Arroyo Rosales <miguel@ruidovisual.com>
2018-10-07 14:52:30 -0500

    critname empty

--- src/components/CRITInfo.vue
+++ src/components/CRITInfo.vue
@@ -3,21 +3,23 @@
     <svg class='crit-illustration'>
       <use :xlink:href="['#icon-center-' + getIllustration()]" />
     </svg>
-    <h2 class='text-emphasis text-center color-primary'>
 
-    <select v-model="selectedCRIT" name="" id="">
-      <option
-        :value="option.name"
-        v-for="option in options"
-      >
-        {{option.name}}
-      </option>
-    </select>
+
     <h2
       style="padding: 30px 0; display: flex; justify-content: center; font-size: 24px; color: #6a1f75; font-weight: bold"
     >
-
+      
     </h2>
+    <div class="text-center">
+      <select v-model="selectedCRIT" name="" id="">
+        <option
+          :value="option.name"
+          v-for="option in options"
+        >
+          {{option.name}}
+        </option>
+      </select>
+    </div>  
 
     <information-nugget
       :lines="[
@@ -61,7 +63,7 @@
 import InformationNugget from './InformationNugget'
 
 export default {
-  name: "CRITInfo",
+  name: "CritInfo",
   data () {
     return { selectedCRIT: null }
   },
